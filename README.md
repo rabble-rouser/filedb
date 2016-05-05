fd2c28286a3ac9fe23c7bb5544ccd9aa647a2386
C. Beninati <cbeninati@rabbleandrouser.com>
2016-05-05 14:38:13 -0600

    updating index and readme

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ A PHP local filesystem data management package.
 ## Installation
 
 Install via Composer.
+
+```
+composer require rabblerouser/filedb
+```
--- src/Utils/CacheResource.php
+++ src/Utils/CacheResource.php
@@ -9,10 +9,15 @@ class CacheResource
 {
     public $cachePath           = '/';
     public $resourceName        = '';
+
     public $transformer;
     protected $transformerClass;
+
     protected $filesystem;
+
+    protected $indexAttribute   = 'id';
     protected $index            = 'id';
+
     protected $extension        = 'json';
 
     public function __construct()
@@ -27,11 +32,11 @@ class CacheResource
         $this->transformer = new $this->transformerClass;
     }
 
-    public function index()
+    public function all()
     {
         $responseArray = array();
         foreach($this->filesystem->listContents() as $file) {
-            $responseArray[$file['filename']] = $this->filesystem->read($file['path']);
+            $responseArray[$file['filename']] = (array)json_decode($this->filesystem->read($file['path']));
         }
 
         return $responseArray;
@@ -40,8 +45,8 @@ class CacheResource
     public function get($name)
     {
         $standard_response = array();
-        if($this->filesystem->has($name . '.json')) {
-            $result = $this->filesystem->read($name . '.json');
+        if($this->filesystem->has($name . $this->extension)) {
+            $result = $this->filesystem->read($name . $this->extension);
             $standard_response = json_decode($result);
         }else {
             // @TODO: Handle no result response. - Chad
@@ -55,9 +60,16 @@ class CacheResource
     {
         $jsonEntity = json_decode($entity);
 
-        $index  = $this->index;
-        $key    = $jsonEntity->$index;
-        $path   = $key . $this->extension;
+        $indexAttribute = $this->indexAttribute;
+        $key            = $jsonEntity->$indexAttribute;
+
+        $path           = $key . $this->extension;
+
+        // Ensure that we have an explicit index attribute.
+        $index = $this->index;
+        if(! isset($jsonEntity->$index)) {
+            $jsonEntity->$index = $key;
+        }
 
         // Write our new dataset to the filesystem. If a dataset
         // already exists for this index, we'll overwrite it.
@@ -78,6 +90,5 @@ class CacheResource
         } else {
             return array('response' => 'No entity could be found at index ' . $entityIndex .'.', 'status' => 'incomplete');
         }
-
     }
 }
